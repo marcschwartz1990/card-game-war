7ea8f4cec1d3ec6b4a1da0e1b961f9cf8a2930db
Marc Schwartz <marcschwartz1990@gmail.com>
2022-04-27 21:53:47 -0400

    Evaluation of cards mostly complete, still need war. Next will need to reduce into functions

--- deckofcards.py
+++ deckofcards.py
@@ -5,6 +5,12 @@ class DeckOfCards:
     """Simulates a deck of playing cards"""
 
     card_values = dict(spades=3, hearts=2, diamonds=1, clubs=0)
+    card_ranks = {'2': 2, '3': 3, '4': 4,
+                  '5': 5, '6': 6, '7': 7,
+                  '8': 8, '9': 9, '10': 10,
+                  'J': 11, 'Q': 12, 'K': 13,
+                  'A': 14,
+                  }
     FULL_DECK = 52
 
     def __init__(self):
@@ -34,3 +40,39 @@ class Player:
 
     def __init__(self):
         self.cards = []
+
+    def get_card(self):
+        return self.cards.pop(0)
+
+
+def play_round(p1_cards, p2_cards):
+    pass
+    #TODO
+    # Make function work for variable number of players
+
+
+def evaluate_cards(p1_card, p2_card, deck, p1, p2, board):
+    if deck.card_ranks[str(p1_card[0])] == deck.card_ranks[str(p2_card[0])]:
+        print('war')
+        reserve = []
+        for i in range(2):
+            reserve.append(board.pop(0))
+        for i in range(3):
+            p1_reserve_card = p1.cards.pop(0)
+            p2_reserve_card = p2.cards.pop(0)
+            reserve.append(p1_reserve_card)
+            reserve.append(p2_reserve_card)
+        p1_card = p1.cards.pop(0)
+        p2_card = p2.cards.pop(0)
+        board.append(p1_card)
+        board.append(p2_card)
+    elif deck.card_ranks[str(p1_card[0])] > deck.card_ranks[str(p2_card[0])]:
+        print('player 1 wins!')
+        p1.cards.append(p1_card)
+        p1.cards.append(p2_card)
+    else:
+        print('player 2 wins!')
+        p2.cards.append(p1_card)
+        p2.cards.append(p2_card)
+    #TODO
+    # Make function work for variable number of players
--- war.py
+++ war.py
@@ -5,8 +5,53 @@ p1 = Player()
 p2 = Player()
 
 players = [p1, p2]
-
 deck.deal_cards(52, players)
+
+# PLAY ROUND
+board = []
+
+# Get top card from each player and place on board.
+p1_card = p1.get_card()
+p2_card = p2.get_card()
+board.append(p1_card)
+board.append(p2_card)
+print(board)
+
+# Evaluate.
+if deck.card_ranks[str(board[0][0])] == deck.card_ranks[str(board[1][0])]:
+    print('war')
+    reserve = []
+    for i in range(2):
+        reserve.append(board.pop(0))
+    for i in range(3):
+        p1_reserve_card = p1.cards.pop(0)
+        p2_reserve_card = p2.cards.pop(0)
+        reserve.append(p1_reserve_card)
+        reserve.append(p2_reserve_card)
+    p1_war_card = p1.cards.pop(0)
+    p2_war_card = p2.cards.pop(0)
+    board.append(p1_war_card)
+    board.append(p2_war_card)
+    print(reserve)
+    print(board)
+    #TODO
+    # Evaluate board at this point. Give winning player all cards from board and reserve.
+elif deck.card_ranks[str(board[0][0])] > deck.card_ranks[str(board[1][0])]:
+    print('player 1 wins!')
+    for card in board:
+        p1.cards.append(card)
+        board = []
+else:
+    print('player 2 wins!')
+    for card in board:
+        p2.cards.append(card)
+        board = []
+
 print(len(p1.cards))
 print(len(p2.cards))
 
+
+    #TODO
+    # Turn PLAY ROUND into series of functions.
+    # Create function for "war"
+    # Create evaluate function
